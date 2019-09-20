4768dded07f2232a9816c41452ce4060f72d5269
H-voyager <15592122178@163.com>
2019-09-20 23:13:54 +0800

    近期总计测试

--- CPP-test/CPP-test/test.cpp
+++ CPP-test/CPP-test/test.cpp
@@ -6,11 +6,15 @@
 #include "sharedptr.h"
 #include <memory>
 #include "BSTree.h"
+#include "RBTree.h"
 #include "AVLTree.h"
+#include <sstream>
 #include "Seqlist.h"
+#include <stack>
 #include <algorithm>
 #include "map.h"
 #include "set.h"
+#include <unordered_map>
 
 //struct fun1
 //{
@@ -522,10 +526,836 @@ struct ListNode
 	~ListNode(){ cout << "~ListNode()" << endl; }
 };
 
+
+class Base
+{
+public:
+	virtual void Func1()
+	{
+		cout << "Base::Func1()" << endl;
+	}
+	virtual void Func2()
+	{
+		cout << "Base::Func2()" << endl;
+	}
+	void Func3()
+	{
+		cout << "Base::Func3()" << endl;
+	}
+private:
+	int _b = 1;
+};
+
+
+class Derive : public Base
+{
+public:
+	virtual void Func1()
+	{
+		cout << "Derive::Func1()" << endl;
+	}
+private:
+	int _d = 2;
+};
+
+class SolutionS {
+public:
+	void reOrderArray(vector<int> &array) {
+		vector<int> even;
+		vector<int> uneven;
+		for (int i = 0; i < array.size(); i++)
+		{
+			if ((array[i] % 2) == 0)
+			{
+				even.push_back(array[i]);
+			}
+			else
+			{
+				uneven.push_back(array[i]);
+			}
+		}
+		for (int i = 0; i < even.size(); i++)
+		{
+			uneven.push_back(even[i]);
+		}
+		for (int i = 0; i < uneven.size(); i++)
+		{
+			cout << uneven[i];
+		}
+	}
+};
+
+class SolutionQ {
+public:
+	vector<int> printMatrix(vector<vector<int> > matrix)
+	{
+		vector<int> ret;
+		if (matrix.empty())return ret;
+
+		int x, y, cnt = matrix[0].size()*matrix.size();
+		int rEdge = matrix[0].size() - 1, dEdge = matrix.size() - 1, lEdge = 0, uEdge = 0;
+
+		for (x = 0, y = 0; cnt>0; cnt--)
+		{
+			ret.push_back(matrix[x][y]);
+
+			if (x == uEdge)
+			{
+				if (y<rEdge)
+					y++;
+				else if (y == rEdge)
+				{
+					x++;
+				}
+				continue;
+			}
+
+			if (y == rEdge)
+			{
+				if (x<dEdge)x++;
+				else if (x == dEdge)
+				{
+					y--;
+				}
+				continue;
+			}
+
+			if (x == dEdge)
+			{
+				if (y>lEdge)y--;
+				else if (y == lEdge)
+				{ 
+					x--;
+				}
+				continue;
+			}
+
+			if (y == lEdge){
+				if (x>uEdge + 1)
+					x--;
+				else if (x == uEdge + 1)
+				{
+					y++;
+					lEdge++;
+					uEdge++;
+					rEdge--;
+					dEdge--;
+				}
+				continue;
+			}
+		}
+		return ret;
+	}
+};
+
+//输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否可能为该栈的弹出顺序
+//假设压入栈的所有数字均不相等。例如序列1,2,3,4,5是某栈的压入顺序，序列4,5,3,2,1是该压栈
+//序列对应的一个弹出序列，但4,3,5,1,2就不可能是该压栈序列的弹出序列。（注意：这两个序列的长度是相等的）
+class SolutionE {
+public:
+	bool IsPopOrder(vector<int> pushV, vector<int> popV) {
+		if (pushV.size() != popV.size())
+			return false;
+
+		stack<int> Stack;
+		for (int i = 0; i < pushV.size(); i++)
+		{
+			Stack.push(pushV[i]);
+		}
+		for (int i = 0; i < popV.size(); i++)
+		{
+			if (popV[i] == Stack.top())
+			{
+				Stack.pop();
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+class SolutionT {
+public:
+	int MoreThanHalfNum_Solution(vector<int> numbers) {
+		int times = numbers.size() / 2;
+		unordered_map<int, int> Map;
+		for (auto e : numbers)
+		{
+			Map[e]++;
+		}
+		for (auto e : Map)
+		{
+			if (e.second > times)
+				return e.first;
+		}
+		return 0;
+	}
+};
+
+
+
+struct TreeNode {
+int val;
+struct TreeNode *left;
+struct TreeNode *right;
+TreeNode(int x) :
+val(x), left(NULL), right(NULL) {
+}
+};
+
+class Solution {
+public:
+	TreeNode* KthNode(TreeNode* pRoot, int k)
+	{
+		if (pRoot == nullptr || k == 0)
+			return nullptr;
+		stack<TreeNode*> Stack;
+		int count = 0;
+		TreeNode* node = pRoot;
+		do{
+			if (node != nullptr){
+				Stack.push(node);
+				node = node->left;
+			}
+			else{
+				node = Stack.top();
+				Stack.pop();
+				count++;
+				if (count == k)
+					return node;
+				node = node->right;
+			}
+		} while (node != nullptr || !Stack.empty());
+		return nullptr;
+
+	}
+};
+
+class SolutionX {
+public:
+	vector<int> GetLeastNumbers_Solution(vector<int> input, int k)
+	{
+		map<int, int> Map;
+		vector<int> res;
+		int size = input.size();
+		for (int i = 0; i < size; i++)
+		{
+			Map.insert(make_pair(input[i],input[i]));
+		}
+		map<int, int> ::iterator it = Map.begin();
+
+		for (int i = 0; i < k; i++)
+		{
+			res.push_back(it->first);
+			it++;
+
+		}
+		return res;
+	}
+};
+
+int compare1(string str1, string str2)//自定义的比较大小的函数，str1 "小"的话，返回true
+{	//if(str1>=str2) then str1+str2 >=str2+str1
+	string tmp1 = str1.append(str2);
+	string tmp2 = str2.append(str1);
+	//cout<<tmp1<<" "<<tmp2<<endl;
+	if (tmp1.compare(tmp2)<0)//
+		return true;
+	return false;
+}
+
+
+class SolutionG {
+public://别人的代码就是精简，惭愧啊，继续学习。
+	int GetUglyNumber_Solution(int index) {
+		if (index < 7)return index;
+		vector<int> res(index);
+		res[0] = 1;
+		int t2 = 0, t3 = 0, t5 = 0, i;
+		for (i = 1; i < index; ++i)
+		{
+			res[i] = min(res[t2] * 2, min(res[t3] * 3, res[t5] * 5));
+			if (res[i] == res[t2] * 2)t2++;
+			if (res[i] == res[t3] * 3)t3++;
+			if (res[i] == res[t5] * 5)t5++;
+		}
+		return res[index - 1];
+	}
+};
+
+class SolutionI {
+public:
+	long long InversePairsCore(vector<int>& data, vector<int>& copy, int start, int end)
+	{
+		if (start == end)
+		{
+			copy[start] = data[start];
+			return 0;
+		}
+
+		int length = (end - start) / 2;
+		long long left = InversePairsCore(copy, data, start, start + length);
+		long long right = InversePairsCore(copy, data, start + length + 1, end);
+
+		//i初始化为前半段最后一个数字的下标
+		int i = start + length;
+		//j初始化为后半段最后一个数字的下标
+		int j = end;
+		int indexCopy = end;
+		long long count = 0;
+		while (i >= start && j <= start + length + 1)
+		{
+			if (data[i] > data[j])
+			{
+				copy[indexCopy--] = data[i--];
+				count += j - start - length;
+			}
+			else
+			{
+				copy[indexCopy--] = data[j--];
+			}
+		}
+		for (; i >= start; i--)
+		{
+			copy[indexCopy--] = data[i];
+		}
+		for (; j >= start +length +1; j--)
+		{
+			copy[indexCopy--] = data[j];
+		}
+		return left + right + count;
+	}
+	int InversePairs(vector<int> data)
+	{
+		if (data.size() == 0)
+		{
+			return 0;
+		}
+		int length = data.size();
+		vector<int> copy;
+		for (int i = 0; i < length; i++)
+		{
+			copy.push_back(data[i]);
+		}
+		long long count = InversePairsCore(data, copy, 0, length - 1);
+		
+		return count % 1000000007;
+	}
+};
+
+class SolutionGN {
+public:
+	int GetNumberOfK(vector<int> data, int k) {
+		unordered_map<int,int> map;
+		for (auto e : data)
+		{
+			map[e]++;
+		}
+		for (auto e : map)
+		{
+			if (e.second == k)
+			{
+				return e.first;
+			}
+		}
+	}
+};
+class SolutionFN {
+public:
+	void FindNumsAppearOnce(vector<int> data, int* num1, int *num2) {
+		vector<int> res;
+		unordered_map<int, int> map;
+		for (auto e : data)
+		{
+			map[e]++;
+		}
+		for (auto e : map)
+		{
+			if (e.second == 1)
+			{
+				res.push_back(e.first);
+			}
+		}
+		num1[0] = res[0];
+		num2[0] = res[1];
+	}
+};
+
+class SolutionLR {
+public:
+	string LeftRotateString(string str, int n) {
+		string tmp;
+		tmp = str.substr(n, str.size() - 1);
+		str.erase(n, str.size() - 1 );
+		tmp += str;
+		return tmp;
+	}
+};
+
+class SolutionIC {
+public:
+	bool IsContinuous(vector<int> numbers) {
+		if (numbers.empty() || numbers.size() != 5 )
+			return false;
+		std::sort(numbers.begin(), numbers.end());
+		for (int i = 0; i < numbers.size() - 1; i++)
+		{
+			int tmp = 0;
+			if (numbers[i] == 0)
+				tmp++;
+
+			tmp = std::abs(numbers[i + 1] - numbers[i]);
+			if (tmp != 1)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+
+class SolutionDL {
+public:
+	// Parameters:
+	//        numbers:     an array of integers
+	//        length:      the length of array numbers
+	//        duplication: (Output) the duplicated number in the array number
+	// Return value:       true if the input is valid, and there are some duplications in the array number
+	//                     otherwise false
+	bool duplicate(int numbers[], int length, int* duplication) {
+		vector<int> v(numbers, numbers + length);
+		map <int, int> map;
+		unordered_map<int, int> umap;
+		multimap<char, int> first;
+
+		struct classcomp {
+			bool operator() (const char& lhs, const char& rhs) const
+			{
+				return lhs<rhs;
+			}
+		};
+		first.insert(std::pair<char, int>('a', 10));
+		first.insert(std::pair<char, int>('b', 15));
+		first.insert(std::pair<char, int>('b', 20));
+		first.insert(std::pair<char, int>('c', 25));
+
+		std::multimap<char, int> second(first.begin(), first.end());
+
+		std::multimap<char, int> third(second);
+
+		std::multimap<char, int, classcomp> fourth;                 // class as Compare
+
+		for (auto e : v )
+		{
+			map[e]++;
+			umap[e]++;
+		}
+		for (auto e : map)
+		{
+			if (e.first == *duplication && e.second > 1)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+
+class solutionIG
+{
+public:
+	multiset<int> rec;
+	void Insert(int num)
+	{
+		rec.insert(num);
+	}
+	double GetMedian()
+	{
+		auto it1 = rec.begin(), it2 = it1;
+		advance(it1, rec.size() / 2);
+		advance(it2, rec.size() / 2 - !(rec.size() % 2));
+		return (*it1 + *it2) / 2.0;
+	}
+	//string function(string& str1, string& str2)
+	//{
+	//	int length1 = str1.size();
+	//	int length2 = str2.size();
+	//	string res;
+	//	for (int i = 0; i < length1; i++)
+	//	{
+	//		for (int j = 0; j < length2; j++)
+	//		{
+	//			if (str1[i] == str2[j])
+	//			{
+	//				
+	//			}
+	//		}
+
+	//	}
+
+	//}
+
+};
+
+class SolutionIF
+{
+public:
+
+	string s;
+	char hash[256];
+	//Insert one char from stringstream
+	void Insert(char ch)
+	{
+		s += ch;
+		hash[ch] += 1;
+	}
+	//return the first appearence once char in current stringstream
+	char FirstAppearingOnce()
+	{
+		int size = s.size();
+		for (int i = 0; i < size; i++){
+			if (hash[s[i]] == 1)
+				return s[i];
+		}
+		return '#';
+	}
+};
+
+class SolutionMA {
+public:
+	vector<int> maxInWindows(const vector<int>& num, unsigned int size)
+	{
+		vector<int> res;
+		deque<int> s;
+		for (unsigned int i = 0; i<num.size(); ++i){
+			while (s.size() && num[s.back()] <= num[i])//从后面依次弹出队列中比当前num值小的元素，同时也能保证队列首元素为当前窗口最大值下标
+				s.pop_back();
+			while (s.size() && i - s.front() + 1>size)//当当前窗口移出队首元素所在的位置，即队首元素坐标对应的num不在窗口中，需要弹出
+				s.pop_front();
+			s.push_back(i);//把每次滑动的num下标加入队列
+			if (size&&i + 1 >= size)//当滑动窗口首地址i大于等于size时才开始写入窗口最大值
+				res.push_back(num[s.front()]);
+		}
+		return res;
+	}
+};
+
+//矩阵中的最短路径
+class SolutionHS {
+public:
+	bool hasPath(char* matrix, int rows, int cols, char* str)
+	{
+		if (matrix == NULL || rows < 1 || cols < 1 || str == NULL)
+			return false;
+		bool *flag = new bool[rows * cols];
+		memset(flag, false, rows * cols);
+		for (int i = 0; i< rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				if (Search(matrix, rows, cols, i, j, str, 0, flag))
+				{
+					return true;
+				}
+			}
+		}
+		delete[] flag;
+		return  false;
+	}
+	bool Search(char* matrix, int rows, int cols, int i, int j, char* str, int k, bool *flag)
+	{
+		int index = i * cols + j;
+		if (i < 0 || i > rows || j > cols || matrix[index] != str[k] || flag[index] == true)
+		{
+			return false;
+		}
+		if (str[k + 1] == '\0')
+			return true;
+		flag[index] = true;
+		if (Search(matrix, rows, cols, i - 1, j, str, k + 1, flag)
+			|| Search(matrix, rows, cols, i + 1, j, str, k + 1, flag)
+			|| Search(matrix, rows, cols, i, j - 1, str, k + 1, flag)
+			|| Search(matrix, rows, cols, i, j + 1, str, k + 1, flag))
+		{
+			return true;
+		}
+		flag[index] = false;
+		return false;
+	}
+
+};
+
 int main()
 {
+	//int a = 1, b = 2, c = 2;
+	//int i = 0;
+	//while (a<b<c) 
+	//{
+	//	int t = a;
+	//	a = b;
+	//	b = t;
+	//	c--;
+	//	i++;
+	//}
+	//printf("%d,%d,%d", a, b, i);
+	char c[5] = { 'a', 'b','\0','c','\o' };
+	printf("%s", c);
 
-	TestAVLTree();
+	//char a[20] = "hello world";
+	//cout << sizeof(a) << endl;
+	//cout << strlen(a) << endl;
+
+	//vector<int> num = { 2, 3, 4, 2, 6, 2, 5, 1 };
+	//int size = 3;
+	//SolutionMA MA;
+	//MA.maxInWindows(num, size);
+	//vector<int> res = MA.maxInWindows(num, size);
+	//vector<int>::iterator it = res.begin();
+	//while (it != res.end())
+	//{
+	//	cout << *it << " ";
+	//	++it;
+	//}
+
+
+	//solutionIG IG;
+	//int num = 0;
+	//while (cin >> num)
+	//{
+	//	IG.Insert(num);
+	//}
+	//double res = IG.GetMedian();
+	//cout << res;
+
+
+
+	//auto_ptr< string> p1(new string("hello world"));
+	//auto_ptr<string> p2;
+	//p2 = p1; //auto_ptr不会报错.
+	////cout << *p1;
+	//cout << *p2;
+
+	//unique_ptr<string> ps1, ps2;
+	//ps1 = unique_ptr<string>(new string("hello"));
+	//ps2 = move(ps1);
+	//ps1 = unique_ptr<string>(new string("alexia"));
+	//cout << *ps2 << *ps1 << endl;
+
+	//int arr[] = { 10, 20, 30 };
+	//int *brr = arr;
+	//cout << *brr << endl;
+	//cout << *(brr + 1) << endl;
+	//cout << *(brr + 2) << endl;
+	//
+	//cout << *(brr) << endl;
+	//cout << *brr + 1 << endl;
+	//cout << *brr + 2 << endl;
+	//char ch1 []= { 'g', 'o', 'o', 'g', 'l', 'e' };
+	//SolutionIF IF;
+	//for (int i = 0; i < (sizeof(ch1)/sizeof(char)); i++)
+	//{
+	//	IF.Insert(ch1[i]);	
+	//}
+	//char res = IF.FirstAppearingOnce();
+	//cout << res;
+
+
+
+
+	//map<int, int> map;
+	//map.insert('2', '1');
+	//int  i = map.size();
+	//cout << i;
+	//int  a[2][3] = { { 1, 2, 3 }, { 4, 5, 6 } }, (*p)[3], *q[3];
+	//p = a;
+	////cout << *p[0]<<*p[0] + 1<<*p[1]<<*p[3];
+	////q = a;//不可赋值
+	//p = &a[1];
+	//q[0] = &a[0][0];//1
+	//q[1] = &a[0][2];//3
+	//q[2] = &a[1][0];//4
+	//q[3] = &a[1][2];//6
+	//cout << *q[0];
+	//cout << *q[1];
+	//cout << *q[2];
+	//cout << *q[3];
+	////q = &a[1];//不可赋值
+
+
+	//int number[] = { 2, 1, 3, 1, 4 };
+	//SolutionDL DL;
+	//int output = 1;
+
+	//int *p = &output;
+	//bool a = DL.duplicate(number, length, p);
+	//cout << a;
+	/*string s = "bcba";
+	int length = s.size()
+	for (int i = 0; i < length; i++)
+	{
+	cout <<s[i] ;
+	}
+	*/
+
+	/*string s("stringvrctor");
+	SolutionLR LR;
+	LR.LeftRotateString(s, 3);
+	string tmp = LR.LeftRotateString(s, 3);
+	for (auto e : tmp)
+	{
+	cout << e;
+	}*/
+
+
+	//SolutionFN I;
+	//int* num1 = 0,* num2 = 0;
+	//int arr[] = { 1, 2, 2, 4, 4, 6, 6, 7 };
+	//vector<int> v(arr, arr + sizeof(arr) / sizeof(int));
+	//I.FindNumsAppearOnce(v,num1,num2);
+
+	//cout << num1[0];
+	//cout << num2[0];
+
+	//char a[100];
+	//while (cin.getline(a,100))
+	//{
+	//	cout << a << endl;
+	//	puts(a);
+	//}
+	//string str;
+	//while (cin >> str)
+	//{
+	//	cout << str << endl;
+	//}
+	//SolutionG G;
+	//cout<<G.GetUglyNumber_Solution(10);
+
+
+
+
+	////取最小数组排列数
+	//int num[] = { 332, 41, 322, 32, 414, 4 };
+	//int len = sizeof(num) / sizeof(int);
+	//cout << "len = " << len << endl;
+	//string *word = new string[len];
+	//stringstream sst;
+	//for (int i = 0; i<len; i++)//讲int整数转化成字符串
+	//{
+	//	sst << num[i];
+	//	sst >> word[i];
+	//	sst.clear();
+	//}
+	//cout << endl;
+	//sort(word, word + len, compare1);//利用泛型算法排序
+	//for (int i = 0; i<len; i++)
+	//{
+	//	cout << word[i];
+	//}
+	//getchar();
+
+	//int a[] = { 1, 2, 3, 2, 4, 2, 5, 2, 3 };
+	//int b[] = { 4, 5, 1, 6, 2, 7, 3, 8 };
+	//vector<int> pushV(a, a + sizeof(a) / sizeof(int));
+	//vector<int> popV(b, b + sizeof(b) / sizeof(int));
+
+
+	//SolutionX X;
+	//vector<int> re = X.GetLeastNumbers_Solution(popV, 4);
+	//vector<int> ::iterator it = re.begin();	
+	//while (it != re.end())
+	//{
+	//	cout << *it;
+	//	it++;
+	//}
+
+
+	//SolutionE E;
+	//E.IsPopOrder(pushV, popV);
+	//	SolutionT T;
+	//	int res = T.MoreThanHalfNum_Solution(pushV);
+
+
+
+
+
+	//SolutionQ Q;
+	//int a[3][3] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	//vector<vector<int>> vv(3,vector<int>(3));
+	//for (int i = 0; i < 3; i++)
+	//{
+	//	for (int j = 0; j < 3; j++)
+	//	{
+	//		vv[i][j] = a[i][j];
+	//	}
+	//}
+	//vector<int> ret = Q.printMatrix(vv);
+	//for (int i = 0; i < ret.size(); i++)
+	//{
+	//	cout << ret[i];
+	//}
+
+
+	//Base b;
+	//Derive d;
+
+	//d.Func1();
+	//d.Func2();
+	//d.Func3();
+
+
+	//string s;
+	//s = "string";
+	//string v("vector");
+	//string l("list map");
+	//vector<string> vs;
+	//vs.push_back(s);
+	//vs.push_back(v);
+	//vs.push_back(l);
+	//auto pos = find(vs.begin(),vs.end(),"vector");
+	////vs.erase(pos);
+	//for (int i = 0; i < vs[1].size(); i++)
+	//{
+	//	cout << vs[1][i];
+	//}
+	//for (auto& e : vs)
+	//{
+	//	cout << e << endl;
+	//}
+
+	//int a[] = { 10, 12, 13, 14 };
+	//cout << &a << endl;
+	//int*ptr = (int*)(&a + 1);
+	//cout << *ptr;
+
+
+	/*	vector<int> in( a ,a+sizeof(a)/sizeof(int));
+		vector<int>::iterator pos1 = find(in.begin(), in.end(), 13);
+		cout << *pos1 << endl;
+
+		for (auto& e : in)
+		{
+		cout << e << endl;
+		}
+		struct sw
+		{
+		int x : 3;
+		int y : 4;
+		int z : 5;
+		double a;
+		};
+		cout << sizeof(sw) << endl*/;
+	//一次申请4G内存
+	//void* p = new char[0xfffffffful];
+	//cout << "new:" << p << endl;
+
+	//TestRBtree();
+
+	//TestAVLTree();
 
 	//TestBSTree();
 
@@ -584,10 +1414,10 @@ int main()
 	//TestString1();
 	/*struct A
 	{
-		long a1;
-		short a2;
-		int a3;
-		int *a4;
+	long a1;
+	short a2;
+	int a3;
+	int *a4;
 	};
 	long a1;
 	cout << sizeof(A) << endl;*/
@@ -619,10 +1449,10 @@ int main()
 	//{
 	//	cout << ' ' << arr[i] << endl;
 	//}
-//	Solution A;
-//	A.oddInOddEvenInEven(arr, arr.capacity());
-//	A.PrintVector(arr);
-//
+	//	Solution A;
+	//	A.oddInOddEvenInEven(arr, arr.capacity());
+	//	A.PrintVector(arr);
+	//
 	//Person p;
 
 	//Date d;
@@ -644,3 +1474,4 @@ int main()
 	system("pause");
 	return 0;
 }
+
