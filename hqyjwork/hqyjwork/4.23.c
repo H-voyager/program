a438fe59fced619a8d03ae85b76a20f7ae8001eb
H-voyager <15592122178@163.com>
2019-10-15 16:02:49 +0800

    智能小车项目练习

--- hqyjwork/hqyjwork/4.23.c
+++ hqyjwork/hqyjwork/4.23.c
@@ -7,19 +7,19 @@
 
 int main()
 {
-	char a[10] = "hello";
-	char b[10] = "helllllo";
-		Compare(a[10], b[10], 10);
-	//int input;
-	//int count;
-	//int array[100] = { 0 };
-	//printf_s("请输入一个正整数:\n");
-	//scanf_s("%d", &input);
+	//char a[10] = "hello";
+	//char b[10] = "helllllo";
+	//	Compare(a[10], b[10], 10);
+	int input;
+	int count;
+	int array[100] = { 0 };
+	printf_s("请输入一个正整数:\n");
+	scanf_s("%d", &input);
 
-	//count = CreateArray(array,input);
-	//ArrayRrverse(array, count);
-	//FindZero(array,count);
-	//PrintArray(array, count);
-	//system("pause");
+	count = CreateArray(array,input);
+	ArrayRrverse(array, count);
+	FindZero(array,count);
+	PrintArray(array, count);
+	system("pause");
 }
 
